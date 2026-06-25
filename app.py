19e17e39eece42f4dcc3e3200e8085507e870c81
Jyothi Maheswaram <maheswaramj@gmail.com>
2026-06-25 14:41:55 +0530

    Removed ollama

--- app.py
+++ app.py
@@ -52,13 +52,6 @@ def get_fertilizer_prediction(
 def init_db():
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS qa (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            question TEXT,
-            answer TEXT
-        )
-    """)
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS history (
@@ -70,41 +63,15 @@ def init_db():
         fertilizer TEXT
     )
 """)
-    cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE,
-    password TEXT
-)
-""")
+    
     conn.commit()
     conn.close()
 
 init_db()
 
-# ---------------- MODEL ----------------
-def predict(question):
-    return f"Answer for: {question}"
-
-# ---------------- SAVE ----------------
-def save_to_db(question, answer):
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO qa (question, answer) VALUES (?, ?)",
-        (question, answer)
-    )
-    conn.commit()
-    conn.close()
 
 # ---------------- GET LAST ENTRY ----------------
-def get_latest():
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT question, answer FROM qa ORDER BY id DESC LIMIT 1")
-    row = cursor.fetchone()
-    conn.close()
-    return row
+
 
 def save_history(crop, temperature, humidity, fertilizer):
 
@@ -152,39 +119,26 @@ def get_history():
 
 # ---------------- ROUTES ----------------
 
-@app.route("/")
-def home():
-    return redirect(url_for("login"))
 
 @app.route("/home")
 def home_page():
 
-    if "user" not in session:
-        return redirect(url_for("login"))
 
     return render_template("index.html")
-
-@app.route("/digitaltwin")
-def digitaltwin():
-
-    if "user" not in session:
-        return redirect(url_for("login"))
-
-    return render_template("digitaltwin.html", farm=latest_farm_data)
 
 @app.route("/fertilizer")
 def fertilizer():
 
-    if "user" not in session:
-        return redirect(url_for("login"))
-
     return render_template("fertilizer.html")
+
+@app.route("/digitaltwin")
+def digitaltwin():
+
+    return render_template("digitaltwin.html", farm=latest_farm_data)
+
 
 @app.route("/history")
 def history():
-
-    if "user" not in session:
-        return redirect(url_for("login"))
 
     records = get_history()
 
@@ -193,77 +147,12 @@ def history():
         records=records
     )
 
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-
-    if request.method == "POST":
-
-        username = request.form["username"]
-        password = request.form["password"]
-
-        conn = sqlite3.connect("database.db")
-        cursor = conn.cursor()
-
-        cursor.execute(
-            "SELECT * FROM users WHERE username=? AND password=?",
-            (username, password)
-        )
-
-        user = cursor.fetchone()
-
-        conn.close()
-
-        if user:
-            session["user"] = username
-            return redirect(url_for("home_page"))
-
-        return "Invalid Username or Password!"
-
-    return render_template("login.html")
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-
-    if request.method == "POST":
-
-        username = request.form["username"]
-        password = request.form["password"]
-        confirm = request.form["confirm_password"]
-
-        if password != confirm:
-            return "Passwords do not match!"
-
-        conn = sqlite3.connect("database.db")
-        cursor = conn.cursor()
-
-        try:
-            cursor.execute(
-                "INSERT INTO users(username, password) VALUES (?, ?)",
-                (username, password)
-            )
-
-            conn.commit()
-
-        except sqlite3.IntegrityError:
-            conn.close()
-            return "Username already exists!"
-
-        conn.close()
-
-        return redirect(url_for("login"))
-
-    return render_template("register.html")
-
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 @app.route("/predict", methods=["POST"])
 def predict_fertilizer():
-
-    if "user" not in session:
-        return redirect(url_for("login"))
 
     crop = request.form["crop"]
     soil = request.form["soil"]
@@ -319,13 +208,6 @@ def predict_fertilizer():
 def result():
     data = get_latest()
     return render_template("result.html", data=data)
-
-@app.route("/logout")
-def logout():
-
-    session.pop("user", None)
-
-    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
